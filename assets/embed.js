00a1cef73aa1aaa4eccbb1d9358f36c73cb46b31
Dennis Rosero <dennisrosero@hotmail.com>
2022-04-24 18:58:24 -0500

    FIXES2
    :hamster:

--- assets/embed.js
+++ assets/embed.js
@@ -7,13 +7,10 @@ function open_full_windowed() {
     localStorage.yfw = "true";
     document.body.classList.add('yfw');
     // window.addEventListener("resize", resize_controls);
-    var theater_mode = document.querySelector('ytd-watch-flexy').getAttribute('theater');
-    if (theater_mode == "") {
-        teatro = true;
-    } else {
-        teatro = false;
-    }
-    if (teatro == false) {
+    // if (document.querySelector('ytd-watch-flexy').theater == false) {
+    console.log(document.querySelector('ytd-watch-flexy').getAttribute('theater'))
+    console.log(document.querySelector('ytd-watch-flexy').theater)
+    if (document.querySelector('ytd-watch-flexy').getAttribute('theater') == null) {
         document.querySelector('.ytp-size-button').click();
     }
     document.getElementById("yfw_button").innerHTML = icon_min;
@@ -28,24 +25,31 @@ function close_full_windowed() {
     document.getElementById("yfw_button").innerHTML = icon_max;
     window.dispatchEvent(new Event('resize'));
     if (document.activeElement != document.body) document.activeElement.blur();
-    // if (document.querySelector('ytd-watch-flexy').theater) {
-    //     teatro = true;
-    // } else {
-    //     teatro = false;
-    // }
-    // if (!teatro) {
-    //     document.querySelector('.ytp-size-button').click();
-    // }
+    if (!teatro) {
+        document.querySelector('.ytp-size-button').click();
+    }
+}
+
+function set_theater() {
+    if (document.querySelector('ytd-watch-flexy').getAttribute('theater') == null) {
+        teatro = false;
+    } else {
+        teatro = true;
+    }
 }
 
 function init() {
     if (yfw_init == false) {
+
         if (localStorage.yfw == undefined) {
             localStorage.yfw = "false";
         }
 
-        //document.body.classList.add('extension-full-windowed');
-        //AÑADIR BOTON
+        set_theater();
+
+        document.querySelector('.ytp-size-button').addEventListener('click', function(e) {
+            set_theater();
+        });
 
         if (document.getElementById("yfw_button")) {
             var button = document.getElementById("yfw_button");
@@ -77,11 +81,23 @@ function init() {
         document.querySelector('.ytp-button.ytp-settings-button').insertAdjacentElement('afterend', button);
         document.body.addEventListener('keyup', function(e) {
             if (location.pathname == "/watch") {
+
+                if (e.keyCode == 84) {
+                    if (localStorage.yfw == "true") {
+                        if (document.querySelector('ytd-watch-flexy').getAttribute('theater') == null) {
+                            document.querySelector('.ytp-size-button').click();
+                        }
+                    }else{
+                        set_theater();
+                    }
+                }
+
                 if (e.keyCode == 27) {
                     if (localStorage.yfw == "true") {
                         close_full_windowed();
                     }
                 }
+
                 if (e.keyCode == 87 && e.shiftKey) {
                     var target = e.target.id;
                     if (target != "search" && target != "contenteditable-root") {
@@ -92,6 +108,7 @@ function init() {
                         }
                     }
                 }
+
             }
 
         });
@@ -149,7 +166,7 @@ function resize_controls() {
 
 // });
 
-//Force init
+//FORCE INIT
 document.body.addEventListener("yt-navigate-start", init);
 document.body.addEventListener("yt-navigate-finish", init);
 setTimeout(function() {
@@ -157,7 +174,6 @@ setTimeout(function() {
 }, 1000);
 
 
-//Media
 var icon_max = '<svg height="100%" viewBox="0 0 36 36" width="100%"><rect x="9" y="18" width="9" height="9" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><polyline points="9 16.2 9 9 13 9" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><line x1="16" y1="9" x2="21.5" y2="9" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><polyline points="23 9 27 9 27 13" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><line x1="27" y1="16" x2="27" y2="21.5" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><polyline points="27 23 27 27 19.8 27" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/></svg>';
 
 var icon_min = '<svg height="100%" viewBox="0 0 36 36" width="100%"><polyline points="18 23.5 18 27 14.5 27" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><polyline points="12.5 27 9 27 9 23.5" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><polyline points="9 21.5 9 18 12.5 18" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><polyline points="14.5 18 18 18 18 21.5" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/><polyline points="9 16.2 9 9 27 9 27 27 19.8 27" style="fill:none;stroke:#fff;stroke-opacity:1;stroke-width:2px;"/></svg>';
